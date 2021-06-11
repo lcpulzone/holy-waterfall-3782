c5cb31c333a6c291742b8585f7a8227a010ec8fd
Leigh <lcpulzone@gmail.com>
2021-06-11 09:38:46 -0700

    Add testing to model and show specs

--- spec/features/airlines/show_spec.rb
+++ spec/features/airlines/show_spec.rb
@@ -6,19 +6,24 @@ RSpec.describe 'airline show page' do
     @dalia = Airline.create!(name: 'Dalia Flights')
 
     @orange = @hawk.flights.create!(number: '7854', date: '05/13/2021', departure_city: 'San Fran', arrival_city: 'Orange County')
-    @redmomd = @dalia.flights.create!(number: '5913', date: '09/13/2021', departure_city: 'Portland', arrival_city: 'Redmond')
+    @redmond = @dalia.flights.create!(number: '5913', date: '09/13/2021', departure_city: 'Portland', arrival_city: 'Redmond')
     @bend = @dalia.flights.create!(number: '0666', date: '04/01/2021', departure_city: 'Seattle', arrival_city: 'Bend')
 
-    @nick = @bend.passengers.create!(name: 'Nick Pulzone', age: 36)
-    @annie = @bend.passengers.create!(name: 'Annie Wesley', age: 18)
-    @matt = @bend.passengers.create!(name: 'Matt Rock', age: 12)
+    @nick = @redmond.passengers.create!(name: 'Nick Pulzone', age: 36)
+    @annie = @redmond.passengers.create!(name: 'Annie Wesley', age: 18)
+    @matt = @redmond.passengers.create!(name: 'Matt Rock', age: 12)
+
+    @bend.passengers << @nick
+    @bend.passengers << @annie
+    @bend.passengers << @matt
   end
 
-  it 'can list passengers that have flights on this airline' do
+  it 'can list only adult passengers that have flights on this airline with no duplicates' do
     visit "/airlines/#{@dalia.id}"
 
-    expect(page).to have_content(@nick.name)
-    expect(page).to have_content(@annie.name)
-    expect(page).to have_content(@matt.name)
+    expect(page).to have_content(@nick.name, count: 1)
+    expect(page).to have_content(@annie.name, count: 1)
+
+    expect(page).to_not have_content(@matt.name)
   end
 end
